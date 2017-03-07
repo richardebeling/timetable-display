4d9521f5f525c6980ef7b709ade4dbef970e89de
Richard Ebeling <dev@richardebeling.de>
2017-03-07 17:23:22 +0100

    Enhanced startscript

--- startscript.sh
+++ startscript.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+# Example rc.local entry:
+# (/bin/sleep 30 && cd /home/pi/dementia-timetable && exec su pi -c './startscript.sh')
+#
 
 echo "Making sure the desktop environment is running..."
-sudo service lightdm start
-echo "Starting dementia timetable..."
-DISPLAY=:0 LANG=de_DE.utf8 ./dt_main.py -f
+service lightdm status &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "! Seems like lightdm is not running !"
+    exit
+fi
+
+
+_SNAME=dt
+tmux has-session -t dt &> /dev/null
+if [ $? -eq 0 ]; then
+    echo "! Seems like the program is already running !"
+    exit
+fi
+
+echo "Starting dementia timetable in a new tmux session..."
+tmux new-session -s $_SNAME -x 140 -y 35 -d
+tmux send-keys 'DISPLAY=:0 LANG=de_DE.utf8 ./dt_main.py -f' Enter
 
